834daff44279c107429926e8f4a6130953df13bb
Ashwin Ramesh <ashwinr64@gmail.com>
2016-10-27 22:06:09 +0530

    onyx: Hack GPS for N
    Change-Id: I4eb0d139a352ecfa1697992c989019781f12ce33

--- gps/core/LocAdapterBase.cpp
+++ gps/core/LocAdapterBase.cpp
@@ -80,7 +80,7 @@ void LocAdapterBase::
 }
 
 void LocAdapterBase::
-    reportSv(QcomGnssSvStatus &svStatus,
+    reportSv(HaxxSvStatus &svStatus,
              GpsLocationExtended &locationExtended,
              void* svExt)
 DEFAULT_IMPL()
